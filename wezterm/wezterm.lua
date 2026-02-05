d1a1f9006fb5620ddbfb834a0f596b5b10991a91
xzx97 <xzx971105@outlook.com>
2026-02-05 15:50:22 +0800

    修改了tmux 中的文件，重新配置了.tmux.conf

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -9,8 +9,15 @@ config = {
 	window_decorations = "RESIZE",
 	default_cursor_style = "BlinkingBar",
 	color_scheme = "Nord (Gogh)",
-	font = wezterm.font("Hack Nerd Font"),
+	-- font = wezterm.font("Hack Nerd Font Mono"),
+	font = wezterm.font_with_fallback({
+    "JetBrainsMono Nerd Font Mono",
+    "Nerd Fonts Symbols",
+	"Devicons",
+	"Noto Color Emoji"
+  }),
 	font_size = 16,
+	warn_about_missing_glyphs=false
 }
 
 return config
